798de5f18663932ab46cd0e81b420e27c9d3a39f
cozyrosy <gracejacob.rose@gmail.com>
2023-05-03 18:10:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react'
+import {action, originals} from './urls'
+import NavBar from './Components/NavBar/NavBar';
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <RowPost url={originals}title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall />  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,4 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY = "8c3ec4be7d60e4ca47c523f6c72cf346"
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
+
